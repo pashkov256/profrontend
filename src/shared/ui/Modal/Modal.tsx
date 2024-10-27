db2aba5ff7008be568278c7342262ba64d334463
temaweb10 <pashkovjob@gmail.com>
2024-10-27 16:02:03 +0300

    fix theme for modal

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,6 +3,7 @@ import React, {
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 
 interface ModalProps {
@@ -27,6 +28,8 @@ export const Modal = (props: ModalProps) => {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
     };
+
+    const { theme } = useTheme();
 
     const closeHandler = useCallback(() => {
         if (onClose) {
