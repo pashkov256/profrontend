b1ed77037942ad84c8190cb4edc961c3eb714959
temaweb10 <pashkovjob@gmail.com>
2024-11-10 09:40:53 +0300

    fix closure error

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -8,10 +8,12 @@ export interface UseInfiniteScroll {
 
 export function useInfiniteScroll({ callback, triggerRef, wrapperRef }:UseInfiniteScroll) {
     useEffect(() => {
+        const wrapperElement = wrapperRef.current;
+        const triggerElement = triggerRef.current;
         let observer:IntersectionObserver | null = null;
         if (callback) {
             const options = {
-                root: wrapperRef.current,
+                root: wrapperElement,
                 rootMargin: '0px',
                 threshold: 1.0,
             };
@@ -22,13 +24,13 @@ export function useInfiniteScroll({ callback, triggerRef, wrapperRef }:UseInfini
                 }
             }, options);
 
-            observer.observe((triggerRef.current));
+            observer.observe(triggerElement);
         }
 
         return () => {
-            if (observer) {
+            if (observer && triggerElement) {
                 // eslint-disable-next-line react-hooks/exhaustive-deps
-                observer.unobserve(triggerRef.current);
+                observer.unobserve(triggerElement);
             }
         };
     }, [triggerRef, wrapperRef]);
