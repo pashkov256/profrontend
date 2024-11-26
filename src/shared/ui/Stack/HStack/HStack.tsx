6cc01ac9d985f0f3fd1eab38c11a406dd2bc4b8b
pashkov256 <pashkovjob@gmail.com>
2024-11-26 10:53:59 +0300

    add HStack,VStack,Flex

---
+++ src/shared/ui/Stack/HStack/HStack.tsx
@@ -0,0 +1,9 @@
+import { Flex, FlexProps } from 'shared/ui/Stack/Flex/Flex';
+
+type HStackProps = Omit<FlexProps, 'direction'>
+
+export const HStack = (props: HStackProps) => {
+    return (
+        <Flex direction="row" {...props} />
+    );
+};
